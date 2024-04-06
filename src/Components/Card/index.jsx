1556085a19867354a33226fa9424090fcf40d015
Gokselagan <gokselagan@outlook.com>
2024-04-06 06:53:40 +0300

    GameRules Added and project done

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,10 +2,10 @@
 import stylesheet from "./styles.css";
 
 
-export const Card = ({card, index}) => {
+export const Card = ({card, index, handleClick}) => {
 
     return (
-        <div className="card">
+        <div className={`card ${card.status}`} onClick={()=>handleClick(index)}>
            <img src={card.img} alt={card.name}/>
         </div>
     )
--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,13 +1,59 @@
+import { useRef, useState } from "react";
 import { Card } from "../Card";
 import { CardImages } from "../CardImages";
 import stylesheet from "./styles.css";
 
 export const Cards = () => {
-  const cards = CardImages.sort(() => Math.random() - .5);
+  const [cards, setCards] = useState(CardImages.sort(() => Math.random() - .5));
+
+  console.log("cart", cards);
+
+  const [prevCardState, setPrevCardState] = useState(-1);
+  const prevIndex = useRef(-1);
+
+  const matchCheck = (currentCard) => {
+    if (cards[currentCard].id === cards[prevCardState].id) {
+      cards[prevCardState].status = "active matched"
+      cards[currentCard].status = "active matched"
+      setPrevCardState(-1)
+    } else {
+      cards[currentCard].status = "active"
+      setCards([...cards]);
+      setTimeout(() => {
+        setPrevCardState(-1);
+        cards[currentCard].status = "unmatch"
+        cards[prevCardState].status = "unmatch"
+        setCards([...cards]);
+      }, 1000);
+    }
+  }
+
+  const handleClick = (index) => {
+
+    if (index !== prevIndex.current) {
+      if (cards[index].status === "active matched") {
+        alert("already matched");
+      } else {
+        if (prevCardState === -1) {
+          prevIndex.current = index;
+          cards[index].status = "active";
+          setCards([...cards]);
+          setPrevCardState(index);
+        } else {
+          matchCheck(index)
+          prevIndex.current = -1;
+        }
+      }
+    } else {
+      alert("card currently selected");
+    }
+
+  }
+
   return (
     <div className="container">
       {cards.map((card, index) => {
-        return <Card card={card} key={index} index={index} />
+        return <Card card={card} key={index} index={index} handleClick={handleClick} />
       })}
     </div>
   )
